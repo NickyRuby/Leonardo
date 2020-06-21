71963f4228981cdc69cd72f52e2559345a3f82c1
NickyRuby <nikita.lobyntsev@gmail.com>
2020-06-21 14:35:23 +0300

    Reminders for everybody

--- reminder.js
+++ reminder.js
@@ -1,14 +1,12 @@
 const leonardo = require('./bot').leonardo;
 const pool = require('./bot').pool;
 
-pool.query('SELECT * FROM Users WHERE user_id=119821330', (err,data)=> {
+pool.query('SELECT * FROM Users', (err,data)=> {
     if (err) {
         throw err;
     } 
-    // console.log(data.rows)
     let message = `Как день? Нажимай /record, чтобы записать результаты 🚀`;
     data.rows.forEach(user => {
-        console.log('')
         leonardo.sendMessage(user.user_id,message);
     })
 });
